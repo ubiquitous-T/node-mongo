aee158b310b7358d8a23066a13759f5008c099a9
Scott Todd <scotts.vocation@gmail.com>
2018-09-10 13:33:08 -0700

    Node DB Example Part 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const dbname = 'conFusion';
@@ -12,6 +13,7 @@ MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
   
   const db = client.db(dbname);
   const collection = db.collection("dishes");
+  /*
   collection.insertOne(({"name": "Mighty Tasty Burgers", "description": "World's tastiest burgers!"}), 
   (err, result) => {
     assert.equal(err, null);
@@ -29,7 +31,34 @@ MongoClient.connect(url, {useNewUrlParser: true}, (err, client) => {
         assert.equal(err, null);
 
         client.close();
-      })
-    })
-  })
+      });
+    });
+  });
+  */
+  dboper.insertDocument(db, { name: "Bacon Cheese Burger", description: "Tasty" },
+      "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);
+
+        dboper.findDocuments(db, "dishes", (docs) => {
+          console.log("Found Documents:\n", docs);
+
+          dboper.updateDocument(db, { name: "Bacon Cheese Burger" },
+              { description: "Very, very tasty" }, "dishes", 
+              (result) => {
+                console.log("Updated Document:\n", result.result);
+
+            dboper.findDocuments(db, "dishes", (docs) => {
+              console.log("Found Updated Documents:\n", docs);
+
+              db.dropCollection("dishes", (result) => {
+                console.log("Dropped Collection: ", result);
+
+                client.close();
+              });
+            });
+          });
+        });
+  });
 });
+
+
